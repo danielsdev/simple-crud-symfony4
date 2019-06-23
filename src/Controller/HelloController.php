9b23623906f37f417904e856c6f4272193443739
Daniel Lima <danielrlima2015@gmail.com>
2019-06-23 04:12:25 -0300

    Formulario de teste

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Produto;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
 class HelloController extends Controller
@@ -54,5 +57,31 @@ class HelloController extends Controller
         $em->flush();
 
         return new Response("O produto {$produto->getId()} foi cafastrado com sucesso");
+    }
+
+    /**
+     * 
+     * @Route("formulario")
+     */
+    public function formulario(Request $request){
+
+        $produto = new Produto();
+
+        $form = $this->createFormBuilder($produto)
+                     ->add('nome', TextType::class)
+                     ->add('preco', TextType::class)
+                     ->add('enviar', SubmitType::class, ['label' => 'Salvar'])
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            return new Response("O formulário está OK!");
+        }
+
+        return $this->render('formulario.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Entity/Produto.php
+++ src/Entity/Produto.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProdutoRepository")
@@ -18,11 +19,13 @@ class Produto
 
     /**
      * @ORM\Column(type="string", length=100)
-     */
+     * @Assert\NotBlank()
+     */ 
     private $nome;
 
     /**
      * @ORM\Column(type="decimal", scale=2)
+     * @Assert\NotBlank()
      */
     private $preco;
 
