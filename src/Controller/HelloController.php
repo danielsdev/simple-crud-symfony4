5b9cfa82759f7f873c9a88b9b6440cc75385a695
Daniel Lima <danielrlima2015@gmail.com>
2019-06-23 01:38:46 -0300

    Instalação das annotations

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,14 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+
+    /**
+     * @return Response
+     * 
+     * @Route("hello_world")
+     */
 
     public function hello(){
 
         return new Response(
             "<html><h1>Hello world</h1></html>"
         );
+
     }
 }
